1fb249ab61a6bf6dc2c93e43d6dc73a751f1d1a5
André Barros <andrebarros0426@gmail.com>
2022-10-01 20:24:00 +0100

    feat: class CountriesWithGreaterProductionconstructor

--- src/main/java/isep/esinf/usecase/CountriesWithGreaterProduction.java
+++ src/main/java/isep/esinf/usecase/CountriesWithGreaterProduction.java
@@ -3,8 +3,34 @@ package isep.esinf.usecase;
 /*
  * Alínea 2.
  */
+
 public class CountriesWithGreaterProduction {
+  String fruit="";
+  int production=0;
+  ArrayList<String> list = new ArrayList<>();
+
+  /*Class constructor */
+  public CountriesWithGreaterProduction(String fruit, int production) {
+    validateFruit(fruit);
+    validateProduction(production);
+  }
+
+
   public void execute() {
     throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  /*Validates if the Fruit is valid, not NULL or EMPTY */
+  private void validateFruit(String fruit) {
+    if (fruit == NULL || fruit == "") throw new UnsupportedOperationException("Fruit invalid.");
+    
+    fruit = this.fruit;
+  }
+
+  /*Validates it the Production is valid, not NULL */
+  private void validateProduction(int number) {
+    if (production == NULL) throw new UnsupportedOperationException("Production invalid.");
+  
+    number = this.production;
   }
 }
