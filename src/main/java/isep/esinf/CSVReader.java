ed613438a2a275d6786317f818223a1a9a8c2460
Tomás Lopes <tomas050302@gmail.com>
2022-10-01 15:52:05 +0100

    fix: fixed csv reader class

--- src/main/java/isep/esinf/CSVReader.java
+++ src/main/java/isep/esinf/CSVReader.java
@@ -2,36 +2,38 @@ package isep.esinf;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Scanner;
 
-public class CSVReader<M extends Map<String,String>, T extends List<M>> {
-    Scanner sc;
-    File file;
-    T list;
-    M map;
+public class CSVReader {
+  Scanner sc;
+  File file;
 
-    public CSVReader(String fileName) throws FileNotFoundException{
-        file = new File(fileName);
-        sc = new Scanner(file);
+  public CSVReader(String fileName) throws FileNotFoundException {
+    file = new File(fileName);
+    sc = new Scanner(file);
+  }
+
+  public String[] readHeader() {
+    return sc.nextLine().split(";");
+  }
+
+  public List<HashMap<String, String>> read() {
+    String[] header = readHeader();
+    List<HashMap<String, String>> list = new ArrayList<>();
+
+    while (sc.hasNextLine()) {
+      HashMap<String, String> map = new HashMap<>();
+
+      String line[] = sc.nextLine().split(";");
+      for (int i = 0; i < header.length; i++)
+        map.put(header[i], line[i]);
+
+      list.add(map);
     }
 
-    public T read() {
-        String[] header = readHeader();
-        
-        while(sc.hasNextLine()){
-            // Generate hasmap
-            String line[] = sc.nextLine().split(";");
-            for (int i = 0; i < header.length; i++) {
-                map.put(header[i], line[i]);
-            }
-            list.add(map);
-        }
-        return list;
-    }
-
-    public String[] readHeader(){
-        return sc.nextLine().split(";");
-    }
+    return list;
+  }
 }
--- src/test/java/isep/esinf/CSVReaderTest.java
+++ src/test/java/isep/esinf/CSVReaderTest.java
@@ -1,43 +1,42 @@
 package isep.esinf;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 class CSVReaderTest {
-    /**
-     * Rigorous Test.
-     * @throws FileNotFoundException
-     */
-    @Test
-    void testReadWorks() throws FileNotFoundException {
-        CSVReader r = new CSVReader<>("./src/test/test.csv");
-        List l = new ArrayList<HashMap<String, String>>();
+  /**
+   * Rigorous Test.
+   *
+   * @throws FileNotFoundException
+   */
+  @Test
+  void testReadWorks() throws FileNotFoundException {
+    List<HashMap<String, String>> l = new ArrayList<HashMap<String, String>>();
+    CSVReader r = new CSVReader("./src/test/test.csv");
 
-        l = r.read();
+    l = r.read();
 
-        HashMap map = new HashMap<String, String>();
-        List expected = new ArrayList<HashMap>();
+    HashMap<String, String> map = new HashMap<String, String>();
+    List<HashMap<String, String>> expected = new ArrayList<HashMap<String, String>>();
 
-        map.put("key1", "123");
-        map.put("key2", "456");
-        map.put("key3", "789");
-        expected.add(map);
+    map.put("key1", "123");
+    map.put("key2", "456");
+    map.put("key3", "789");
+    expected.add(map);
 
-        map = new HashMap<String, String>();
-        map.put("key1", "abd");
-        map.put("key2", "def");
-        map.put("key3", "ghi");
-        expected.add(map);
+    map = new HashMap<String, String>();
+    map.put("key1", "abc");
+    map.put("key2", "def");
+    map.put("key3", "ghi");
+    expected.add(map);
 
-        assertEquals(l, expected);
-    }
+    assertEquals(expected, l);
+  }
 }
