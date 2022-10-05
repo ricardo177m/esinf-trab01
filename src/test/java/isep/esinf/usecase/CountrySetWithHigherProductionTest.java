92e0195d5ab19bfe07a9b355ae2dc2cc8de777d1
Ricardo <rmoreiradev@gmail.com>
2022-10-05 02:17:03 +0100

    feat(ex3): added tests

--- src/test/java/isep/esinf/usecase/CountrySetWithHigherProductionTest.java
+++ src/test/java/isep/esinf/usecase/CountrySetWithHigherProductionTest.java
@@ -1,6 +1,7 @@
 package isep.esinf.usecase;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import isep.esinf.model.Container;
@@ -36,18 +37,50 @@ public class CountrySetWithHigherProductionTest {
     fruitData1.addCountryData("Portugal", countryData3);
 
     FruitData fruitData2 = new FruitData();
-    fruitData2.addCountryData("Portugal", countryData1);
+    fruitData2.addCountryData("Italy", countryData1);
 
     container.addFruitData("Maçã", fruitData1);
     container.addFruitData("Pêra", fruitData2);
   }
 
   @Test
-  public void test01() {
-    CountrySetWithHigherProduction countrySetWithHigherProduction = new CountrySetWithHigherProduction(container, 1000);
+  public void testExpectedValue01() {
+    CountrySetWithHigherProduction countrySetWithHigherProduction = new CountrySetWithHigherProduction(container, 900);
     int result = countrySetWithHigherProduction.execute();
     int expected = 2;
 
     assertEquals(expected, result);
+  }
+
+  @Test
+  public void testExpectedValue02() {
+    CountrySetWithHigherProduction countrySetWithHigherProduction = new CountrySetWithHigherProduction(container, 899);
+    int result = countrySetWithHigherProduction.execute();
+    int expected = 1;
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void testExpectedValue03() {
+    CountrySetWithHigherProduction countrySetWithHigherProduction = new CountrySetWithHigherProduction(container, 1600);
+    int result = countrySetWithHigherProduction.execute();
+    int expected = 3;
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void testExceedQuantity() {
+    CountrySetWithHigherProduction countrySetWithHigherProduction = new CountrySetWithHigherProduction(container, 9999999);
+    int result = countrySetWithHigherProduction.execute();
+    int expected = -1;
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void testInvalidQuantity() {
+    assertThrows(IllegalArgumentException.class, () -> new CountrySetWithHigherProduction(container, -1));
   }
 }
