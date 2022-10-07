0e1d96e890a23802196efb3561c0273f182c79d8
carlos <1211277@isep.ipp.pt>
2022-10-07 14:03:37 +0100

    fix: handling not available data

--- src/main/java/isep/esinf/usecase/DataHandler.java
+++ src/main/java/isep/esinf/usecase/DataHandler.java
@@ -19,48 +19,51 @@ public class DataHandler {
       CountryData cd = new CountryData();
       FruitData fd = new FruitData();
 
-      // Verifies if year is defined
-      int year = 0;
-      if(data.get(i).get("Year") != null){
-        year = Integer.parseInt(data.get(i).get("Year"));
-      }else{
-        throw new MissingFieldException("Area field is required.");
-      }
+      if(data.get(i).get("Flag") != "M"){
 
-      int quantity = 0;
-      if(data.get(i).get("Value") != null) quantity = Integer.parseInt(data.get(i).get("Value"));
-      
-      // Verifies if country is defined
-      String country = "";
-      if(data.get(i).get("Area") != null){
-        country = data.get(i).get("Area");
-      }else{
-        throw new MissingFieldException("Area field is required.");
-      }
+        // Verifies if year is defined
+        int year = 0;
+        if(data.get(i).get("Year") != null){
+          year = Integer.parseInt(data.get(i).get("Year"));
+        }else{
+          throw new MissingFieldException("Area field is required.");
+        }
 
-      // Verifies if fruit is defined
-      String fruit = "";
-      if(data.get(i).get("Item") != null){
-        fruit = data.get(i).get("Item");
-      }else{
-        throw new MissingFieldException("Item field is required.");
-      }
+        int quantity = 0;
+        if(data.get(i).get("Value") != null) quantity = Integer.parseInt(data.get(i).get("Value"));
+        
+        // Verifies if country is defined
+        String country = "";
+        if(data.get(i).get("Area") != null){
+          country = data.get(i).get("Area");
+        }else{
+          throw new MissingFieldException("Area field is required.");
+        }
 
-      if (container.contains(fruit)) {
-        fd = container.getFruitData(fruit);
-        if (fd.contains(country)) {
-          cd = fd.getCountryData(country);
-          cd.addProductionData(year, quantity);
+        // Verifies if fruit is defined
+        String fruit = "";
+        if(data.get(i).get("Item") != null){
+          fruit = data.get(i).get("Item");
+        }else{
+          throw new MissingFieldException("Item field is required.");
+        }
+
+        if (container.contains(fruit)) {
+          fd = container.getFruitData(fruit);
+          if (fd.contains(country)) {
+            cd = fd.getCountryData(country);
+            cd.addProductionData(year, quantity);
+          } else {
+            cd.addProductionData(year, quantity);
+            fd.addCountryData(country, cd);
+          }
         } else {
           cd.addProductionData(year, quantity);
           fd.addCountryData(country, cd);
+          container.addFruitData(fruit, fd);
+          }
         }
-      } else {
-        cd.addProductionData(year, quantity);
-        fd.addCountryData(country, cd);
-        container.addFruitData(fruit, fd);
       }
-    }
 
     return container;
   }
